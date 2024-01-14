966a27c4fe3fa2d04643441a1a74ed0cda1bda3b
manpreetsingh04m <ms2404mk@gmail.com>
2024-01-14 23:13:34 +0530

    Done

---
+++ hof/src/App.jsx
@@ -0,0 +1,13 @@
+import HigherOrderComponent from './components/HigherOrderComponent' 
+import'./App.css'
+
+function App() {
+
+  return (
+    <>
+      <HigherOrderComponent/>
+    </>
+  )
+}
+
+export default App
